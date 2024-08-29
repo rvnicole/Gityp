bfa7283c8c2a6c0b17fbe97e2bf839e12189d97b
nicole <nicolerosasvallejo@hotmail.com>
2024-08-28 23:16:39 -0600

    Funcionalidad Emisores/Receptores

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -124,3 +124,7 @@ export const GestionCobroFormSchema = GestionCobrosSchema.pick({
     pagado: true,
     comentarios: true
 });
+
+// Validaciones
+export const EmisoresReceptoresSchema = z.array( EmisorReceptorSchema );
+export const OrdenesServiciosSchema = z.array(OrdenServicioSchema);
