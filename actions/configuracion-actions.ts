6588be2859637e2791c8a25f96d304526a7dc0f3
nicole <nicolerosasvallejo@hotmail.com>
2024-09-11 17:16:31 -0600

    Configuración de Rutas

--- actions/configuracion-actions.ts
+++ actions/configuracion-actions.ts
@@ -1,14 +1,23 @@
 "use server";
 
+import { connectDB } from "@/config/db";
 import { Configuracion, IConfiguracion } from "@/model/Configuracion";
+import { Ruta } from "@/src/types";
 
 export async function getConfig(){
     try{
+        await connectDB(); 
+
         const config = await Configuracion.find<IConfiguracion>();
+        
         if( config.length > 0 ){
             return {
                 folioInicial: config[0].folioInicial,
-                plantillas: config[0].plantillas
+                plantillas: config[0].plantillas,
+                rutas: {
+                    ordenesCompra: config[0].rutas.ordenesCompra,
+                    facturas: config[0].rutas.facturas
+                }
             };
         }
         else{
@@ -16,7 +25,8 @@ export async function getConfig(){
             await config.save();
             return {
                 folioInicial: config.folioInicial,
-                plantillas: []
+                plantillas: [],
+                rutas: {ordenesCompra: '', facturas: ''}
             };
         }
     }
@@ -30,6 +40,7 @@ export async function getConfig(){
 
 export async function setFolioInicial( formData: { folio: string } ){
     try{
+        await connectDB();
         const { folio } = formData;
         const config = await Configuracion.find();
         if( config.length > 0 ){
@@ -51,5 +62,31 @@ export async function setFolioInicial( formData: { folio: string } ){
             success: false,
             message: typeof error === 'object' && error !== null && 'message' in error ? error.message : 'Error al obtener el folio'
         }
+    }
+}
+
+export async function updateRutas( {ruta, tipo}: Ruta ){
+    try{
+        await connectDB();  
+
+        const config = await Configuracion.find();
+
+        if( config.length > 0 ){
+            config[0].rutas[tipo] = ruta;
+            await config[0].save();
+        }
+        else {
+            const config = new Configuracion({rutas: {[tipo]: ruta}});
+            await config.save();
+        }
+
+        return {success: true, message: 'Ruta Actualizada'};
+    }
+    catch(error){
+        if (typeof error === 'object' && error !== null && 'message' in error) {
+            return { success: false, message: error.message}
+        }      
+        
+        return { success: false, message: 'Error al Actualizar la Ruta'}
     }
 }
